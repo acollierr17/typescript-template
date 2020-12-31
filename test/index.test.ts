a9b8cd97156a5f3398e8d4b9a2f540852061fc20
acollierr17 <gamemaniac101@gmail.com>
2020-12-30 22:10:26 -0500

    :rotating_light: Fix linter warnings

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 it('should log "hello world" in the console', () => {
-  console.log = jest.fn()
-  console.log("hello world")
-  expect(console.log).toHaveBeenCalledWith('hello world')
-})
+  console.log = jest.fn();
+  console.log('hello world');
+  expect(console.log).toHaveBeenCalledWith('hello world');
+});
